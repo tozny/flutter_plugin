957ffc19e77855fbade615d286fa35698752e273
Matthew Rhea <matthew@tozny.com>
2021-07-14 16:15:39 -0700

    [WASH-929] Add docs and serialization methods
    - Adds development documentation for setting up and
      running the Flutter iOS plugin
    - Adds FlutterConfigSerializer to bridge Flutter Dart
      client's ClientCredentials object to E3db's Config
    - Adds RealmConfigSerializer
    - Adds encoding and decoding methods for E3db objects
    
    Note that this PR removed an incomplete version of the
    writeRecord method.

--- ios/Classes/SwiftFlutterPlugin.swift
+++ ios/Classes/SwiftFlutterPlugin.swift
@@ -10,57 +10,12 @@ public class SwiftFlutterPlugin: NSObject, Flutter.FlutterPlugin {
         registrar.addMethodCallDelegate(instance, channel: channel)
     }
 
-
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         switch call.method {
         case "getPlatformVersion":
             result("iOS" + UIDevice.current.systemVersion)
-        case "writeRecord":
-            result(self.writeRecord(call, result: result))
         default:
             result(FlutterMethodNotImplemented)
-        }
-    }
-    
-    /// Initializer to create a client from context of `FlutterMethodCall`
-    ///
-    /// A `FlutterConfig` object is necessary to bridge the Flutter client's `ClientCredentials`
-    /// to an E3DB `Config` object.
-    public func initClientFromFlutter(_ call: FlutterMethodCall, result: @escaping FlutterResult) -> Client? {
-        if let args = call.arguments as? Dictionary<String, Any>,
-           let clientCredentialJson = args["client_credentials"] as? Dictionary<String, String> {
-            let jsonData = try? JSONSerialization.data(withJSONObject: clientCredentialJson, options: .prettyPrinted)
-            let jsonString = String(data: jsonData!, encoding: .utf8)
-            let data = jsonString?.data(using: .utf8)
-            let flutterConfig: FlutterConfig = try! JSONDecoder().decode(FlutterConfig.self, from: data!)
-            let config: Config = FlutterConfig.encodeToE3dbConfig(flutterConfig: flutterConfig)
-            let client: Client = Client(config: config)
-            return client
-        }
-        return nil // TODO: Fix optional return value.
-    }
-    
-    // MARK: WRITE RECORD
-    
-    /// Writes a record using a `Client` built with values from the `FlutterMethodCall` object that
-    /// persisted across `FlutterMethodChannel`.
-    ///
-    public func writeRecord(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-        let client: Client = self.initClientFromFlutter(call, result: result)!
-        if let args = call.arguments as? Dictionary<String, Any>,
-           let fields = args["data"] as? [String: String],
-           let plain = args["plain"] as? [String: String],
-           let recordType = args["type"] as? String {
-            do {
-                let recordData: RecordData = RecordData(cleartext: fields)
-                client.write(type: recordType, data: recordData, plain: plain) { (writeResult) in
-                    if let record = writeResult.value {
-                        result(E3dbSerializer.recordToJson(record: record))
-                    } else {
-                        result(FlutterError(code: "WRITE", message: "Write data record failed", details: nil)) // TODO: ERROR
-                    }
-                }
-            }
         }
     }
 }
